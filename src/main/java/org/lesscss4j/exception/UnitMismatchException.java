e4e751d2e78361452596cca507c89b221a22f8fb
David Hay <dhay@localmatters.com>
2010-04-27 21:55:51 +0000

    Actually do something with the units in a UnitMismatchException
    git-svn-id: https://subversion.corp.localmatters.com/subversion/DestinationSearch/lesscss4j/trunk@114719 cb919951-1609-0410-8833-993d306c94f7

--- src/main/java/org/lesscss4j/exception/UnitMismatchException.java
+++ src/main/java/org/lesscss4j/exception/UnitMismatchException.java
@@ -18,18 +18,38 @@ package org.lesscss4j.exception;
 import org.lesscss4j.model.expression.ConstantValue;
 
 public class UnitMismatchException extends ExpressionException {
+    private ConstantValue _left;
+    private ConstantValue _right;
+
     public UnitMismatchException(ConstantValue left, ConstantValue right) {
+        super(left.toString() + " " + right.toString());
+        _left = left;
+        _right = right;
     }
 
     public UnitMismatchException(String message, ConstantValue left, ConstantValue right) {
-        super(message);
+        super(message + ": " + left.toString() + " " + right.toString());
+        _left = left;
+        _right = right;
     }
 
     public UnitMismatchException(String message, ConstantValue left, ConstantValue right, Throwable cause) {
-        super(message, cause);
+        super(message + ": " + left.toString() + " " + right.toString(), cause);
+        _left = left;
+        _right = right;
     }
 
     public UnitMismatchException(ConstantValue left, ConstantValue right, Throwable cause) {
-        super(cause);
+        super(left.toString() + " " + right.toString(),cause);
+        _left = left;
+        _right = right;
+    }
+
+    public ConstantValue getLeft() {
+        return _left;
+    }
+
+    public ConstantValue getRight() {
+        return _right;
     }
 }
