ea48f65861ffd59e80f8ae5fe63bd1524e38e03b
David Hay <dhay@localmatters.com>
2010-04-30 14:42:21 +0000

    Servlet to compile LESS files, cache and serve the resulting CSS.
    git-svn-id: https://subversion.corp.localmatters.com/subversion/DestinationSearch/lesscss4j/trunk@115105 cb919951-1609-0410-8833-993d306c94f7

---
+++ src/test/java/org/lesscss4j/servlet/LessCssServletTest.java
@@ -0,0 +1,311 @@
+/**
+ * File: LessCssServletTest.java
+ *
+ * Author: David Hay (dhay@localmatters.com)
+ * Creation Date: Apr 29, 2010
+ * Creation Time: 2:19:23 PM
+ *
+ * Copyright 2010 Local Matters, Inc.
+ * All Rights Reserved
+ *
+ * Last checkin:
+ *  $Author$
+ *  $Revision$
+ *  $Date$
+ */
+package org.lesscss4j.servlet;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+import junit.framework.TestCase;
+
+import org.apache.commons.io.output.ByteArrayOutputStream;
+import org.easymock.EasyMock;
+
+public class LessCssServletTest extends TestCase {
+    LessCssServlet _servlet;
+    HttpServletRequest _request;
+    HttpServletResponse _response;
+    ServletContext _servletContext;
+    MockServletConfig _servletConfig;
+    String _path = "path/to/test.less";
+    String _cssStr = "body{color:blue;}";
+    byte[] _cssBytes;
+
+    long _systemMillis;
+
+    @Override
+    protected void setUp() throws Exception {
+        _request = EasyMock.createMock(HttpServletRequest.class);
+        _response = EasyMock.createMock(HttpServletResponse.class);
+        _servletContext = EasyMock.createMock(ServletContext.class);
+        
+        _servletConfig = new MockServletConfig();
+        _servletConfig.setServletContext(_servletContext);
+
+        _systemMillis = System.currentTimeMillis();
+
+        _cssBytes = _cssStr.getBytes("UTF-8");
+
+        _servlet = new LessCssServlet() {
+            @Override
+            protected long getTime() {
+                return _systemMillis;
+            }
+        };
+    }
+
+    protected void doReplay() {
+        EasyMock.replay(_request);
+        EasyMock.replay(_response);
+        EasyMock.replay(_servletContext);
+    }
+
+    protected void doVerify() {
+        EasyMock.verify(_request);
+        EasyMock.verify(_response);
+        EasyMock.verify(_servletContext);
+    }
+
+    public void testEmptyCacheValidResource() throws IOException, ServletException {
+
+        EasyMock.expect(_request.getPathInfo()).andReturn(_path);
+        EasyMock.expect(_request.getMethod()).andReturn("GET");
+        EasyMock.expect(_request.getDateHeader(LessCssServlet.IF_MOD_SINCE)).andReturn(-1L);
+        EasyMock.expect(_request.getParameter(LessCssServlet.CLEAR_CACHE)).andReturn(null);
+
+        ByteArrayInputStream input = new ByteArrayInputStream(_cssBytes);
+        EasyMock.expect(_servletContext.getResourceAsStream(_path)).andReturn(input);
+
+        _response.addDateHeader(LessCssServlet.LAST_MODIFIED, _systemMillis);
+        _response.addDateHeader(LessCssServlet.EXPIRES, _systemMillis + 900000);
+        _response.addHeader(LessCssServlet.CACHE_CONTROL, "max-age=900");
+        _response.setContentType("text/css; charset=UTF-8");
+        _response.setContentLength(_cssBytes.length);
+
+        MockServletOutputStream responseStream = new MockServletOutputStream();
+        EasyMock.expect(_response.getOutputStream()).andReturn(responseStream);
+
+        doReplay();
+
+        _servlet.init(_servletConfig);
+        _servlet.service(_request, _response);
+
+        assertEquals(new String(responseStream.getBytes(), "UTF-8"), _cssStr);
+
+        doVerify();
+    }
+
+    public void testCachedResource() throws IOException, ServletException {
+        testEmptyCacheValidResource();
+
+        EasyMock.reset(_request);
+        EasyMock.reset(_response);
+        EasyMock.reset(_servletContext);
+
+        EasyMock.expect(_request.getPathInfo()).andReturn(_path);
+        EasyMock.expect(_request.getMethod()).andReturn("GET");
+        EasyMock.expect(_request.getDateHeader(LessCssServlet.IF_MOD_SINCE)).andReturn(-1L);
+        EasyMock.expect(_request.getParameter(LessCssServlet.CLEAR_CACHE)).andReturn(null);
+
+
+        _response.addDateHeader(LessCssServlet.LAST_MODIFIED, _systemMillis);
+        _response.addDateHeader(LessCssServlet.EXPIRES, _systemMillis + 900000);
+        _response.addHeader(LessCssServlet.CACHE_CONTROL, "max-age=900");
+        _response.setContentType("text/css; charset=UTF-8");
+        _response.setContentLength(_cssBytes.length);
+
+        MockServletOutputStream responseStream = new MockServletOutputStream();
+        EasyMock.expect(_response.getOutputStream()).andReturn(responseStream);
+
+        doReplay();
+
+        _servlet.init(_servletConfig);
+        _servlet.service(_request, _response);
+
+        assertEquals(new String(responseStream.getBytes(), "UTF-8"), _cssStr);
+        
+        doVerify();
+    }
+
+    public void testCachedResourceNeedsRefresh() throws IOException, ServletException {
+        _servlet.setCacheMillis(10);
+
+        testEmptyCacheValidResource();
+
+        _systemMillis = _systemMillis - 20;
+
+        EasyMock.reset(_request);
+        EasyMock.reset(_response);
+        EasyMock.reset(_servletContext);
+
+        EasyMock.expect(_request.getPathInfo()).andReturn(_path);
+        EasyMock.expect(_request.getMethod()).andReturn("GET");
+        EasyMock.expect(_request.getDateHeader(LessCssServlet.IF_MOD_SINCE)).andReturn(-1L);
+        EasyMock.expect(_request.getParameter(LessCssServlet.CLEAR_CACHE)).andReturn(null);
+
+        EasyMock.expect(_servletContext.getRealPath(_path)).andReturn(_path);
+
+        ByteArrayInputStream input = new ByteArrayInputStream(_cssBytes);
+        EasyMock.expect(_servletContext.getResourceAsStream(_path)).andReturn(input);
+
+        _response.addDateHeader(LessCssServlet.LAST_MODIFIED, _systemMillis);
+        _response.addDateHeader(LessCssServlet.EXPIRES, _systemMillis + 900000);
+        _response.addHeader(LessCssServlet.CACHE_CONTROL, "max-age=900");
+        _response.setContentType("text/css; charset=UTF-8");
+        _response.setContentLength(_cssBytes.length);
+
+        MockServletOutputStream responseStream = new MockServletOutputStream();
+        EasyMock.expect(_response.getOutputStream()).andReturn(responseStream);
+
+        doReplay();
+
+        _servlet.init(_servletConfig);
+        _servlet.service(_request, _response);
+
+        assertEquals(new String(responseStream.getBytes(), "UTF-8"), _cssStr);
+
+        doVerify();
+    }
+
+    public void testCachedResourceNotModified() throws IOException, ServletException {
+        testEmptyCacheValidResource();
+
+        EasyMock.reset(_request);
+        EasyMock.reset(_response);
+        EasyMock.reset(_servletContext);
+
+        EasyMock.expect(_request.getPathInfo()).andReturn(_path);
+        EasyMock.expect(_request.getMethod()).andReturn("GET");
+        EasyMock.expect(_request.getDateHeader(LessCssServlet.IF_MOD_SINCE)).andReturn(_systemMillis + 1000);
+        EasyMock.expect(_request.getParameter(LessCssServlet.CLEAR_CACHE)).andReturn(null);
+
+
+        _response.setStatus(HttpServletResponse.SC_NOT_MODIFIED);
+
+        doReplay();
+
+        _servlet.init(_servletConfig);
+        _servlet.service(_request, _response);
+
+        doVerify();
+    }
+
+    public void testCachedResourceHeadRequest() throws IOException, ServletException {
+        testEmptyCacheValidResource();
+
+        EasyMock.reset(_request);
+        EasyMock.reset(_response);
+        EasyMock.reset(_servletContext);
+
+        EasyMock.expect(_request.getPathInfo()).andReturn(_path);
+        EasyMock.expect(_request.getMethod()).andReturn("HEAD");
+        EasyMock.expect(_request.getParameter(LessCssServlet.CLEAR_CACHE)).andReturn(null);
+
+
+        _response.addDateHeader(LessCssServlet.LAST_MODIFIED, _systemMillis);
+        _response.addDateHeader(LessCssServlet.EXPIRES, _systemMillis + 900000);
+        _response.addHeader(LessCssServlet.CACHE_CONTROL, "max-age=900");
+        _response.setContentType("text/css; charset=UTF-8");
+        _response.setContentLength(_cssBytes.length);
+
+        doReplay();
+
+        _servlet.init(_servletConfig);
+        _servlet.service(_request, _response);
+
+        doVerify();
+    }
+
+    public void testNullPath() throws IOException, ServletException {
+        EasyMock.expect(_request.getPathInfo()).andReturn(null);
+        EasyMock.expect(_request.getParameter(LessCssServlet.CLEAR_CACHE)).andReturn(null);
+        _response.sendError(HttpServletResponse.SC_NOT_FOUND);
+
+        doReplay();
+
+        _servlet.service(_request, _response);
+
+        doVerify();
+    }
+
+    public void testEmptyPath() throws IOException, ServletException {
+        EasyMock.expect(_request.getPathInfo()).andReturn("  ");
+        EasyMock.expect(_request.getParameter(LessCssServlet.CLEAR_CACHE)).andReturn(null);
+        _response.sendError(HttpServletResponse.SC_NOT_FOUND);
+
+        doReplay();
+
+        _servlet.service(_request, _response);
+
+        doVerify();
+    }
+
+    private static class MockServletConfig implements ServletConfig {
+        private String _servletName;
+        private ServletContext _servletContext;
+        private Map<String, String> _initParameters = new LinkedHashMap<String, String>();
+
+        public String getServletName() {
+            return _servletName;
+        }
+
+        public ServletContext getServletContext() {
+            return _servletContext;
+        }
+
+        public void setServletName(String servletName) {
+            _servletName = servletName;
+        }
+
+        public void setServletContext(ServletContext servletContext) {
+            _servletContext = servletContext;
+        }
+
+        public void setInitParameter(String name, String value) {
+            _initParameters.put(name, value);
+        }
+
+        public String getInitParameter(String name) {
+            return _initParameters.get(name);
+        }
+
+        public Enumeration getInitParameterNames() {
+            return new Enumeration() {
+                Iterator _iter = _initParameters.keySet().iterator();
+
+                public boolean hasMoreElements() {
+                    return _iter.hasNext();
+                }
+
+                public Object nextElement() {
+                    return _iter.next();
+                }
+            };
+        }
+    }
+
+    private static class MockServletOutputStream extends ServletOutputStream {
+        private ByteArrayOutputStream output = new ByteArrayOutputStream();
+
+        @Override
+        public void write(int b) throws IOException {
+            output.write(b);
+        }
+
+        public byte[] getBytes() {
+            return output.toByteArray();
+        }
+    }
+}
